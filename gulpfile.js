91b97faf57841ec031959a4c86bf01fa35038d85
carloshlessa <you@example.comcarloshclessa@gmail.com>
2021-02-18 10:58:47 +0000

    first final changes, MVP Presentation User Group London

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,77 @@
 // Load plugins
 
-const browsersync = require("browser-sync").create();
+const browsersync = require('browser-sync').create();
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const del = require("del");
+const del = require('del');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-
 
 // BrowserSync
 
 function browserSync(done) {
-    browsersync.init({
-      server: {
-        baseDir: "./"
-      },
-      port: 3000,
-      startPath: './css/outsystems-ui.css'
-    });
-    done();
-  }
+  browsersync.init({
+    server: {
+      baseDir: './',
+    },
+    port: 3000,
+    startPath: './css/outsystems-ui.css',
+  });
+  done();
+}
 
 // BrowserSync reload
 
 function browserSyncReload(done) {
-    browsersync.reload();
-    done();
-  }
-
+  browsersync.reload();
+  done();
+}
 
 // Clean vendor
 function clean() {
-    return del(["./vendor/", "./css/"]);
-  }
-
-
+  return del(['./vendor/', './css/']);
+}
 
 // SCSS to CSS compilator
 
-function style(){
-  
-    // 1. Define the input file (scss)
-    return gulp.src('./scss/*.scss')
+function style() {
+  // 1. Define the input file (scss)
+  return (
+    gulp
+      .src('./scss/*.scss')
 
-    // 2. Pass it through sass compiler with map
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    
-    // 3. Add auto prefixes
-    .pipe(autoprefixer({
-        browserslistrc: ['last 3 versions', 'ie >= 10', '> 5%'],
-        cascade: false
-      })
-    )
-    
-    // 4. Save the compiled CSS
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(
-      gulp.dest('./css')
-    )
+      // 2. Pass it through sass compiler with map
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+
+      // 3. Add auto prefixes
+      .pipe(
+        autoprefixer({
+          browserslistrc: ['last 3 versions', 'ie >= 10', '> 5%'],
+          cascade: false,
+        }),
+      )
+
+      // 4. Save the compiled CSS
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest('./css'))
+  );
 }
 
 // Watching the changes on files
 
 function watchFiles() {
-    gulp.watch("./**/*.css", browserSyncReload);
-    gulp.watch("./**/*.html", browserSyncReload);
-    gulp.watch('./scss/',style);
-  }
-  
+  gulp.watch('./**/*.css', browserSyncReload);
+  gulp.watch('./**/*.html', browserSyncReload);
+  gulp.watch('./scss/', style);
+}
 
 // Defining the tasks
 const vendor = gulp.series(clean);
 const build = gulp.series(vendor);
-const watch = gulp.series(build ,gulp.parallel(watchFiles,browserSync,style));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync, style));
 
-// Exporting the tasks 
+// Exporting the tasks
 
 exports.style = style;
 exports.clean = clean;
